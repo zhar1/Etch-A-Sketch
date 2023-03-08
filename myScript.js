ffa07cd5577333c8a5a336a98af8e3173d55d93f
Zaire <110312083+zhar1@users.noreply.github.com>
2023-03-08 15:13:41 -0600

    will changes color of divs upon hover

--- myScript.js
+++ myScript.js
@@ -1,7 +1,9 @@
 const container = document.createElement("div");
+const button = document.createElement('button');
 container.classList.add('container');
+document.body.appendChild(button);
 document.body.appendChild(container);
-
+button.textContent = 'select number of boxes'
 //creates n number of divs for grid layout
 let numOfDivs = 256;
 let divArray = new Array();
@@ -14,3 +16,12 @@ function createDivs(numOfDivs){
     }
 }
 createDivs(numOfDivs);
+function addBackgroundColor(e){
+    this.style.backgroundColor = 'black';
+}
+divArray.forEach(div => div.addEventListener('mouseover', addBackgroundColor));
+
+button.addEventListener('click', addBoxes);
+function addBoxes(){
+    console.log('yay');
+}
