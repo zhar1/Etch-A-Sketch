7c5197945adc9045e16c8c662aad5797c7df432c
Zaire <110312083+zhar1@users.noreply.github.com>
2023-03-10 10:33:52 -0600

    fix removeGrid function

--- myScript.js
+++ myScript.js
@@ -6,7 +6,7 @@ document.body.appendChild(container);
 button.textContent = 'select number of boxes';
 
 //array to store n number of grids
-let divArray = new Array();
+const divArray = new Array();
 
 // creates column_squared number of divs i.e. column * column
 function createGrid(numOfColumns){
@@ -22,12 +22,15 @@ function createGrid(numOfColumns){
 
 function addBackgroundColor(e){
     this.style.backgroundColor = 'black';
-    console.log(this);
 }
 //const divs = document.querySelectorAll('.gridbox')
 //divs.forEach(div => div.addEventListener('click', addBackgroundColor));
 
 button.onclick = function(){
+    //check for existing grid
+    if(divArray[0]){
+        removeGrid();
+    }
     //prompt for number of columns
     let newNumOfColumns = Number(prompt('Enter number of tiles'));
     //variable to store additions to class 'container'
@@ -40,3 +43,9 @@ button.onclick = function(){
 
 
 /* next we need to add grid attributes by js (number of cololumns) */
+function removeGrid(){
+    divArray.forEach(div => div.remove());
+}
+function resetColor(){
+    divArray.forEach(div => div.style.backgroundColor = 'white');
+}
